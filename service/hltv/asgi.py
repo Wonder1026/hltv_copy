c1e31a9fe8d217c0b0732151302c876289da6ede
Oleg Ivashchenko <email@gmail.com>
2023-03-18 10:30:23 +0300

    added main app and black formatter

--- service/hltv/asgi.py
+++ service/hltv/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hltv.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hltv.settings")
 
 application = get_asgi_application()
